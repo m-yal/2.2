a1b72ce71721dcf6fa0602d3d18a1fdbe17edce7
m-yal <maksym.yalovitsa@gmail.com>
2022-07-25 23:05:30 +0300

    task2

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cross_fetch_1 = __importDefault(require("cross-fetch")); //used https://openbase.com/js/cross-fetch  instead
-async function getMyIp(url) {
-    try {
-        await (0, cross_fetch_1.default)(url)
-            .then(data => {
-            return data.text();
-        })
-            .then(result => {
-            console.log(result);
-        });
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
 const url = "https://api.ipify.org/?format=json";
-getMyIp(url);
+async function getIP(url) {
+    const res = await (0, cross_fetch_1.default)(url);
+    const data = await res.text();
+    console.log(data);
+    return data;
+}
+const ip = getIP(url);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import fetch from 'cross-fetch';//used https://openbase.com/js/cross-fetch  instead
 
-async function getMyIp(url:string) {
-    try {
-        await fetch(url) //used https://developer.mozilla.org/ru/docs/Web/API/Fetch_API/Using_Fetch#%D0%B7%D0%B0%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%B8
-            .then(data =>  {
-                return data.text();
-            })
-            .then(result => {
-                console.log(result);
-            });
-    } catch (error) {
-        console.error(error);
-    }
-    
+const url = "https://api.ipify.org/?format=json";
+
+
+async function getIP(url:string): Promise<string> {
+    const res = await fetch(url);
+    const data = await res.text();
+    console.log(data);
+    return data;
 }
 
-const url = "https://api.ipify.org/?format=json";
-getMyIp(url);
+const ip = getIP(url);
